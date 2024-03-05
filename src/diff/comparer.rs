3e6d792902af68ea994fff09e2e1ae6d6dbb16f1
Daniel van Dijk <daniel@stentorian.io>
2024-03-06 00:11:14 +0100

    feat(pkg): check browser and exports entries
    Refs: #20

--- src/diff/comparer.rs
+++ src/diff/comparer.rs
@@ -2,15 +2,11 @@ use crate::diff::assets::{diff_pkg_assets, diff_pkg_entries};
 use crate::pkg::Pkg;
 
 pub fn count_breaking_changes_between(previous_pkg: Pkg, current_pkg: Pkg) -> anyhow::Result<u32> {
-    let previous_contents = previous_pkg.contents;
-    let previous_main_entry = previous_pkg.entries.main;
-
-    let current_contents = current_pkg.contents;
-    let current_main_entry = current_pkg.entries.main;
-
     let diff_results = vec![
-        diff_pkg_assets(&previous_contents, &current_contents)?,
-        diff_pkg_entries(&previous_main_entry, &current_main_entry)?,
+        diff_pkg_assets(&previous_pkg.contents, &current_pkg.contents)?,
+        diff_pkg_entries(&previous_pkg.entries.main, &current_pkg.entries.main)?,
+        diff_pkg_entries(&previous_pkg.entries.browser, &current_pkg.entries.browser)?,
+        diff_pkg_entries(&previous_pkg.entries.exports, &current_pkg.entries.exports)?,
     ];
 
     Ok(diff_results.into_iter().sum::<u32>())
